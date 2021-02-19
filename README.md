fff85e50bbe74a91e19ed049e3222e1c6b224b88
unknown <dostoievskiab@users.noreply.github.com>
2021-02-18 23:00:46 -0300

    Quick changes

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Next.JS Simple Blog
 ## Introduction
-Making a simple blog platform to learn about some technologies i haven't work before:
+Making a simple blog app to learn about some technologies i haven't work before:
 + NextJS
 + SQLite
 
 ## Installation
-1. Copy the repo with `git clone ` 
+1. Copy the repo with `git clone git@github.com:dostoievskiab/nextjs-simple-blog.git` 
 2. Install packages with `npm install`
 3. Run code using `npm run dev`
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import layoutStyle from '../styles/Layout.module.css'
 import navbarStyle from '../styles/Navbar.module.css'
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 export default function simpleBlog({ Component, pageProps }) {
-    return (<div className={layoutStyle.container}>
-        <div className={layoutStyle.navegationBar}>
-            <img className={navbarStyle.perfilImg} src="coffe.png"></img>
-            <p>Dostoievski Batista</p>
+    return (
+        <div className={layoutStyle.container}>
+            <div className={layoutStyle.navegationBar}>
+                <img className={navbarStyle.perfilImg} src="coffe.png"></img>
+                <p>Dostoievski Batista</p>
+                <a href="https://github.com/dostoievskiab" ><GitHubIcon /></a>
+            </div>
+            <div className={layoutStyle.content}><Component {...pageProps} /></div>
         </div>
-        <div className={layoutStyle.content}><Component {...pageProps} /></div>
-    </div>
     )
 }
